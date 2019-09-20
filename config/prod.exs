e101e1b61e9427e0fb590caebeb1479a8f6fe7d5
awedd95 <awedd95@gmail.com>
2019-09-21 00:40:11 +1000

    Adde Elm dep

--- config/prod.exs
+++ config/prod.exs
@@ -21,17 +21,30 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :resume, ResumeWeb.Endpoint,
+     config :resume, ResumeWeb.Endpoint,
 #       ...
 #       url: [host: "example.com", port: 443],
-#       https: [
-#         :inet6,
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-#       ]
-#
+       #       https: [
+         #         :inet6,
+         #         port: 443,
+         #         cipher_suite: :strong,
+         #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+         #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
+         #       ]
+         #
+         #        config :platform, PlatformWeb.Endpoint,
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [scheme: "https", host: "resumestaging.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  
+  config :resume, ResumeWeb.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+  #
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
 # and clients may not be supported. You can set it to
