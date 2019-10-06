142bb913d755f6c89e12c3e1e5921b66ed11f646
awedd95 <awedd95@gmail.com>
2019-10-06 21:48:46 +1100

    Update

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,9 +12,10 @@ database_url =
     """
 
 config :resume, Resume.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
   ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
